6fcf3e571b4367c175c27a0022bd0b4927013595
Yashika <soniyashika164@gmail.com>
2026-01-26 22:26:45 +0530

    Update report routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
+import reportRoutes from "./routes/reportRoutes.js"
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -11,5 +12,5 @@ app.use(cors({
     credentials:true
 }));
 app.use("/api/auth",authRoutes)
-
+app.use("/api/reports", reportRoutes);
 export default app;
---
+++ Backend/src/routes/reportRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createReport, getReports, updateReportStatus, voteReports } from '../controllers/reportController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+router.post("/", protect, createReport);
+router.get("/", getReports);
+router.patch("/:id/vote", protect, voteReports);
+// router.patch("/:id/status",protect,updateReportStatus);
+
+export default router;
